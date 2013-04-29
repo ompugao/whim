595df8a7296bb8a57fe50eeedfb3789a820be6c3
Shohei Fujii <fujii.shohei@gmail.com>
2013-04-30 00:31:06 +0900

    add all rails files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Whim::Application.config.secret_token = '7c993c0d95d78adda69774372fde11464d0605657e906faaba2c0cbbf03c6cbce973f232a6c334bf61c07017722a18f473e3fad1fa051c61abdde78ced6a2a04'
